3d43fd384948ecfedd216ca84b7568ffa22fa372
Eternal <'jiangtaoScar@163.com'>
2017-03-26 15:23:26 +0800

    搜索框

---
+++ SYF/js/seek.js
@@ -0,0 +1,24 @@
+/*搜索框置顶*/
+// // 获取滚动条先下滚动的距离
+//              var scrollTop = document.getElementsByClassName(".seek").scrollTop();
+//              console.log(scrollTop);
+//              // 移动图片的位置
+////              $("img").offset({top:scrollTop+50});
+$(function() {
+	$(window).scroll(function() {
+//		// 获取滚动条先下滚动的距离
+//		var scrollTop = $(this).scrollTop();
+//		console.log(scrollTop);
+//		var top = $(".seek").offset().top;
+//		if(scrollTop > top){
+////			alert(true);
+//			$(".seek").css("position","fixed").css("margin-top","-72px");
+//		}
+//		if(scrollTop <= top){
+//			$(".seek").css("position","absolute");
+//		}
+//		console.log(top);
+//		// 移动图片的位置
+////		$("img").offset({ top: scrollTop + 50 });
+	})
+})
